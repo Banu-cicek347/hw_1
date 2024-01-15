2d54efe0ad6975e55695ac4a48a923d77f7d36b3
senan <frankinwteyn666@gmail.com>
2024-01-15 05:26:30 -0800

    yeni fayl yarandi

---
+++ hw#1.py
@@ -0,0 +1,58 @@
+"""
+- Printing -
+A. Write a simple program that prints "Python is the beast!"""
+# print("Python is the beast!")
+
+"""B. Write a simple program that prints "I love coding."""
+# print("I love coding.")
+
+"""C. Write a simple program that prints "I am going to master Python soon"""
+# print("I am going to master Python soon")
+
+"""D. Write a simple program that prints "I am learning Python, hooray!!"""
+# print("I am learning Python, hooray!!")
+
+"""E. Write a simple program that prints "Python is the most powerful programming language!"""
+# print("Python is the most powerful programming language!")
+
+"""- Variables -
+A. Create a variable named "fruit" and assign any fruit-related string to it. Print that
+variable."""
+# fruits = "Orange, Apple, Lemon, Watermelon,  Banana, Blueberry, Grape, Mango, Apricot, Cherry"
+# print(fruits)
+
+"""B. Create a variable named "sentence" and assign any string to it. Print that
+variable."""
+# sentence = "I love PYTHON!"
+# print(sentence)
+
+"""C. Create a variable named "name" and assign any name-related string to it. Print that
+variable."""
+# name = "Leonor"
+# print(name)
+
+"""D. Create a variable named "surname" and assign any name-related string to it. Print that
+variable."""
+# surname = "Borbon"
+# print(surname)
+
+"""E. Create a variable named "full_name" and assign any name-related string to it. Print that
+variable."""
+# full_name = "Leonor Borbon"
+# print(full_name)
+
+"""F. Create a variable named "colour" and assign any colour-related string to it. Print that
+variable."""
+# colors = "Red, Rreen, Blue, Black, Pink, Brown, Purple, Yellow, Aqua"
+# print(colors)
+
+"""G. Create a variable named "gender" and assign any gender-related string to it. Print that
+variable."""
+# genders = "Male and Female"
+# print(genders)
+
+"""H. Create a variable named "brand" and assign any brand-related string to it. Print that
+variable.
+"""
+# brands = "Mercedes, BMW, KIA, Ford, Chevrolet, Audi, Toyota, Bentley, Honda, Cadillac, Lamborghini"
+# print(brands)
